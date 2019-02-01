ab12559e13c31228fba15572afea428606c3e9b0
Netopya <netopya@netopyaplanet.com>
2019-01-31 22:19:11 -0500

    Add navigation bar
    -Added nav elements for home, scout, and collector pages
    -Set active state when switching tabs

--- www/js/pages.js
+++ www/js/pages.js
@@ -2,19 +2,24 @@ document.addEventListener("deviceready", initPages, false);
 
 function initPages() {
 	$('.js-splash-screen').remove();
+	$('body').removeClass('is-app-loading');
 	$('[data-role="page"]#main').show();
 
 	let $all_pages = $('[data-role="page"]');
+	let $navigation_tabs = $('.js-navbar-item');
 
 	$all_pages.each((index, element) => {
 		let id = element.id;
 		let $element = $(element);
 
-		$(`[href="#${id}"`).on('click', () => {
+		$(`[href="#${id}"]:not(.dropdown-toggle)`).on('click', () => {
 			$all_pages.hide();
 			$element.show();
 
 			$(document).trigger('page-show.' + id);
+			$navigation_tabs.removeClass('active');
+			$navigation_tabs.filter(`[data-active-page="#${id}"]`).addClass('active');
+
 
 			return false;
 		});
