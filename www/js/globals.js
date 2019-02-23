149e398b878c26efb349797851bf0c3d587a0079
Netopya <netopya@netopyaplanet.com>
2019-02-22 21:55:48 -0500

    [NOTASK] Refactored EventCode's to be full Event's

--- www/js/globals.js
+++ www/js/globals.js
@@ -1,12 +1,44 @@
 class AppState {
 	constructor() {
-		let stored_event = localStorage.getItem('current_event');
+		let stored_events = JSON.parse(localStorage.getItem('events') || null);
+		let stored_current_event = JSON.parse(localStorage.getItem('current_event') || null);
 
-		this.default_event = !stored_event;
-		this.current_event = ko.observable(new EventCode(JSON.parse(stored_event) || undefined));
+		this.default_event = !stored_current_event;
+
+		if (stored_events) {
+			this.events = ko.observableArray(stored_events.map(event => new Event(event)));
+		} else {
+			this.events = ko.observableArray();
+		}
+
+		this.current_event = ko.observable(new Event(this.findEvent(stored_current_event) || undefined));
 		this.current_event.subscribe((event) => {
 			this.default_event = false;
-			localStorage.setItem('current_event', ko.toJSON(event))
+			localStorage.setItem('current_event', JSON.stringify({
+				event_key: event.event_key(),
+				event_year: event.event_year()
+			}));
 		});
+
+		this.events.subscribe(events => {
+			localStorage.setItem('events', ko.toJSON(events));
+		});
+	}
+
+	setCurrentEvent(event) {
+		let existing_event = this.findEvent(event);
+
+		if (existing_event) {
+			this.current_event(existing_event);
+			return existing_event;
+		}
+
+		this.events.push(event);
+		this.current_event(event);
+		return event;
+	}
+
+	findEvent(event) {
+		return this.events().find(stored_event => stored_event.equalsEvent(event))
 	}
 }
