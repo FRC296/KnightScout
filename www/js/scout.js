a7522d9428cb550d5ee9f94a0f38a6ef69822b76
Netopya <netopya@netopyaplanet.com>
2019-02-24 19:45:24 -0500

    Added persistence of scout information

--- www/js/scout.js
+++ www/js/scout.js
@@ -1,19 +1,33 @@
 class Scout {
 	constructor(app_state) {
-		this.scout_name = ko.observable();
-		this.current_sheet = ko.observable(new Sheet(app_state.current_event()));
-		this.sheets = ko.observableArray();
+		let stored_scout = JSON.parse(localStorage.getItem('scout') || null);
+
+		this.scout_name = ko.observable(stored_scout && stored_scout.scout_name || '');
+		this.current_sheet = ko.observable(new Sheet(ko.toJS(app_state.current_event)));
+		this.sheets = ko.observableArray(((stored_scout && stored_scout.sheets) || []).map(sheet => new Sheet(sheet)));
 
 		let self = this;
 		this.editSheet = function() {
 			self.current_sheet(this);
 			window.goToPage('scout-sheet');
 		}
+
+		this.scout_name.subscribe(() => {
+			$(document).trigger('persist_scout');
+		});
+
+		this.sheets.subscribe(() => {
+			$(document).trigger('persist_scout');
+		});
+
+		$(document).on('persist_scout', () => {
+			localStorage.setItem('scout', ko.toJSON(this));
+		});
 	}
 
 	submitSheet() {
 		this.sheets.push(this.current_sheet());
-		this.current_sheet(new Sheet(window.app_state.current_event()));
+		this.current_sheet(new Sheet(ko.toJS(window.app_state.current_event)));
 		window.goToPage('scout-page');
 	}
 
--- www/js/sheet.js
+++ www/js/sheet.js
@@ -27,21 +27,21 @@ const LOCATION_MAPPING = [
 ];
 
 class Action {
-	constructor() {
-		this.action_name = ko.observable();
-		this.action_name_formatted = ko.computed(() => ACTION_MAPPING[this.action_name()], this);
-		this.game_piece = ko.observable();
-		this.game_piece_formatted = ko.computed(() => GAME_PIECE_MAPPING[this.game_piece()], this);
-		this.location = ko.observable();
-		this.location_formatted = ko.computed(() => LOCATION_MAPPING[this.location()], this);
+	constructor(action = {}) {
+		this.action_name = ko.observable(action.action_name);
+		this.action_name.formatted = ko.computed(() => ACTION_MAPPING[this.action_name()], this);
+		this.game_piece = ko.observable(action.game_piece);
+		this.game_piece.formatted = ko.computed(() => GAME_PIECE_MAPPING[this.game_piece()], this);
+		this.location = ko.observable(action.location);
+		this.location.formatted = ko.computed(() => LOCATION_MAPPING[this.location()], this);
 	}
 }
 
 
 class ActionMenu {
-	constructor() {
+	constructor(action_menu = {}) {
 		this.current_action = ko.observable(new Action());
-		this.actions = ko.observableArray();
+		this.actions = ko.observableArray((action_menu.actions || []).map(action => new Action(action)));
 		this.current_menu = ko.observable('home');
 	}
 
@@ -63,29 +63,29 @@ class ActionMenu {
 }
 
 class Sheet {
-	constructor(current_event) {
-		this.scout_name = ko.observable();
-		this.event_key = ko.observable(current_event ? current_event.event_key() : undefined);
-		this.event_year = ko.observable(current_event ? current_event.event_year() : undefined);
-		this.match_number = ko.observable();
-		this.match_level = ko.observable();
-		this.team_number = ko.observable();
-		this.alliance = ko.observable();
+	constructor(sheet_data = {}) {
+		this.scout_name = ko.observable(sheet_data.scout_name);
+		this.event_key = ko.observable(sheet_data.event_key);
+		this.event_year = ko.observable(sheet_data.event_year);
+		this.match_number = ko.observable(sheet_data.match_number);
+		this.match_level = ko.observable(sheet_data.match_level);
+		this.team_number = ko.observable(sheet_data.team_number);
+		this.alliance = ko.observable(sheet_data.alliance);
 
-		this.starts_with = ko.observable();
-		this.auton_bonus = ko.observable();
-		this.auton_mobility = ko.observable();
+		this.starts_with = ko.observable(sheet_data.starts_with);
+		this.auton_bonus = ko.observable(sheet_data.auton_bonus);
+		this.auton_mobility = ko.observable(sheet_data.auton_mobility);
 
-		this.defense_count = ko.observable(0);
+		this.defense_count = ko.observable(sheet_data.defense_count || 0);
 
-		this.end_platform = ko.observable();
-		this.climb_speed = ko.observable();
-		this.carried = ko.observable(false);
+		this.end_platform = ko.observable(sheet_data.end_platform);
+		this.climb_speed = ko.observable(sheet_data.climb_speed);
+		this.carried = ko.observable(sheet_data.carried || false);
 
-		this.robot_speed = ko.observable();
-		this.comments = ko.observable();
+		this.robot_speed = ko.observable(sheet_data.robot_speed);
+		this.comments = ko.observable(sheet_data.comments);
 
-		this.action_menu = new ActionMenu();
+		this.action_menu = new ActionMenu(sheet_data.action_menu);
 	}
 
 	addDefenseInteraction() {
